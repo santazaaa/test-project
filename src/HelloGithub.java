52ddadd27ec433d894baeb365fb97e65ff1e6f98
ArchSanta <santazaaa@gmail.com>
2014-06-24 16:21:38 +0700

    add a new method

--- src/HelloGithub.java
+++ src/HelloGithub.java
@@ -3,6 +3,9 @@ public class HelloGithub {
 	public static void main(String[] args) {
 		System.out.println("Hello Github!");
 		System.out.println("Make somechange");
-		
+	}
+	
+	public void newMethod() {
+		System.out.println("Add a new method!");
 	}
 }
